ff77db1f8ef3194737ef6d0ac65e0f4b5b4b0d85
Ryan Munro <felix@skylab.org>
2011-01-06 00:26:39 -0800

    Heck yes!!!! Rotating triangle!

--- src/org/trevyan/lzr/Game.java
+++ src/org/trevyan/lzr/Game.java
@@ -1,60 +1,74 @@
 package org.trevyan.lzr;
 
-import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
+import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.util.glu.GLU.*;
 
 public class Game {
-  public static final String GAME_TITLE = "Lazors";
-  private static boolean finished;
+  private boolean done = false;
+  public float rot = 0.0f;
+
+  public Game() {
+    init();
+
+    while (!done) {
+      if (Display.isCloseRequested() || Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
+        done = true;
+      render();
+      Display.update();
+    }
+
+    Display.destroy();
+  }
+
+  private void render() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    glTranslatef(-1.5f, 0.0f, -6.0f);
+    glRotatef(rot += 0.75f, 0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+      glColor3f(1.0f, 0.0f, 0.0f);
+      glVertex3f(0.0f, 1.0f, 0.0f);
+      glColor3f(0.0f, 1.0f, 0.0f);
+      glVertex3f(-1.0f, -1.0f, 0.0f);
+      glColor3f(0.0f, 0.0f, 1.0f);
+      glVertex3f(1.0f, -1.0f, 0.0f);
+    glEnd();
+  }
+
+  private void init() {
+    int w = 640;
+    int h = 480;
+
+    try {
+      Display.setDisplayMode(new DisplayMode(w, h));
+      Display.setVSyncEnabled(true);
+      Display.setTitle("Shader Setup");
+      Display.create();
+    } catch (Exception e) {
+      System.out.println("Error setting up display");
+      System.exit(0);
+    }
+
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, ((float) w / (float) h), 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glShadeModel(GL_SMOOTH);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+  }
 
   public static void main(String[] args) {
-    try {
-      // Init
-      Display.setTitle(GAME_TITLE);
-      Display.setDisplayMode(new DisplayMode(640, 480));
-      Display.setVSyncEnabled(true);
-      Display.create();
-      Display.setLocation(100, 100);
-      glMatrixMode(GL_PROJECTION);
-
-      // Run
-      while (!finished) {
-        Display.update();
-
-        // Check for close requests
-        if (Display.isCloseRequested()
-            || Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
-          finished = true;
-        }
-        else
-        {
-          // Draw yo
-          glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-          glLoadIdentity();
-          //glTranslatef(-1.5f,0.0f,-6.0f);
-          glBegin(GL_TRIANGLES);
-            glColor3f(1.0f,0.0f,0.0f);
-            glVertex3f( 0.0f, 1.0f, 0.0f);
-            glColor3f(0.0f,1.0f,0.0f);
-            glVertex3f(-1.0f,-1.0f, 0.0f);
-            glColor3f(0.0f,0.0f,1.0f);
-            glVertex3f( 1.0f,-1.0f, 0.0f);
-          glEnd();
-          Display.sync(30);
-        }
-      }
-    // Exception
-    } catch (Exception e) {
-      e.printStackTrace(System.err);
-      Sys.alert(GAME_TITLE, "An error occured and the game will exit.");
-    // Cleanup
-    } finally {
-      Display.destroy();
-    }
-    System.exit(0);
+    new Game();
   }
 }
+
